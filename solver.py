f13b60d43c49e80790c24c5f0b12774830c31604
Diego Camargo <dicamargov@gmail.com>
2016-05-02 08:18:04 -0500

    removed solver class since it didn't make much sense, started bfs for phase_one

--- solver.py
+++ solver.py
@@ -2,171 +2,153 @@
 # encoding=utf-8
 from treelib import Node, Tree
 from collections import deque
-from copy import deepcopy
 from rubik import Rubik
 from sets import Set
 
-class Solver(object):
-	"""Rubik's Cube Solver"""
-	rubik = None
-	moves = {}
-	phase_one_moves = {}
-	phase_two_moves = {}
-	phase_three_moves = {}
-	phase_four_moves = {}
 
-	def __init__(self, rubik):
-		self.rubik = rubik
-		self.moves = {
-			'u': [self.rubik.move_u],
-			'd': [self.rubik.move_d],
-			'l': [self.rubik.move_l],
-			'r': [self.rubik.move_r],
-			'l2': [self.rubik.move_l, self.rubik.move_l],
-			'r2': [self.rubik.move_r, self.rubik.move_r],
-			'u2': [self.rubik.move_u, self.rubik.move_u],
-			'd2': [self.rubik.move_d, self.rubik.move_d],
-			'f2': [self.rubik.move_f, self.rubik.move_f],
-			'b2': [self.rubik.move_b, self.rubik.move_b]
+rubik = None
+moves = {}
+phase_one_moves = {}
+phase_two_moves = {}
+phase_three_moves = {}
+phase_four_moves = {}
+
+def get_moves(rubik, type):
+	moves = {
+		'u': [rubik.move_u],
+		'd': [rubik.move_d],
+		'l': [rubik.move_l],
+		'r': [rubik.move_r],
+		'l2': [rubik.move_l, rubik.move_l],
+		'r2': [rubik.move_r, rubik.move_r],
+		'u2': [rubik.move_u, rubik.move_u],
+		'd2': [rubik.move_d, rubik.move_d],
+		'f2': [rubik.move_f, rubik.move_f],
+		'b2': [rubik.move_b, rubik.move_b]
+	}
+	if type == 'phase_one':
+		return {
+			'u': moves['u'],
+			'd': moves['d'],
+			'l': moves['l'],
+			'r': moves['r'],
+			'f2': moves['f2'],
+			'b2': moves['b2']
 		}
 
-		self.phase_one_moves = {
-			'u': self.moves['u'],
-			'd': self.moves['d'],
-			'l': self.moves['l'],
-			'r': self.moves['r'],
-			'f2': self.moves['f2'],
-			'b2': self.moves['b2']
+	elif type == 'phase_two':
+		return {
+			'u': moves['u'],
+			'd': moves['d'],
+			'l2': moves['l2'],
+			'r2': moves['r2'],
+			'f2': moves['f2'],
+			'b2': moves['b2']
 		}
 
-		self.phase_two_moves = {
-			'u': self.moves['u'],
-			'd': self.moves['d'],
-			'l2': self.moves['l2'],
-			'r2': self.moves['r2'],
-			'f2': self.moves['f2'],
-			'b2': self.moves['b2']
+	elif type == 'phase_three' or type == 'phase_four':
+		return {
+			'u2': moves['u2'],
+			'd2': moves['d2'],
+			'l2': moves['l2'],
+			'r2': moves['r2'],
+			'f2': moves['f2'],
+			'b2': moves['b2']
 		}
+	
+def solve(rubik, debug=False):
+	needed_moves = []
+	if debug:
+		rubik.describe()
 
-		self.phase_three_moves = {
-			'u2': self.moves['u2'],
-			'd2': self.moves['d2'],
-			'l2': self.moves['l2'],
-			'r2': self.moves['r2'],
-			'f2': self.moves['f2'],
-			'b2': self.moves['b2']
-		}
+	for move in phase_one(rubik, needed_moves, debug):
+		needed_moves.append(move)
 
-		self.phase_four_moves = self.phase_three_moves
-		
-	def solve(self, debug=False):
-		needed_moves = []
-		if debug:
-			rubik.describe()
+	return needed_moves
 
-		for move in self.phase_one(rubik, needed_moves, debug):
-			needed_moves.append(move)
+def create_cube_from_node(tree, node_id):
+	return Rubik(tree.get_node(node_id).identifier)
 
-		for move in self.phase_two(rubik, debug):
-			needed_moves.append(move)
+def init_phase(rubik, steps_required = []):
+	tree = Tree()
+	tree.add_node(Node(identifier = rubik.copy(), tag = {'steps': steps_required}))
+	return tree
 
-		return needed_moves
+def execute(instructions, step):
+	for instruction in instructions[step]:
+		instruction()
+	return step
 
-	def create_cube_from_node(self, tree, node_id):
-		return Rubik(faces = tree.get_node(node_id).tag['state'])
+def add_movements(tree, parent_node_id, phase, states):	
+	moves = get_moves(tree.get_node(parent_node_id).identifier, phase).keys()
+	for move in moves:
+		rubik = tree.get_node(parent_node_id).identifier.copy()
+		step = execute(get_moves(rubik, phase), move)
+		steps = tree.get_node(parent_node_id).tag['steps'][:]
+		steps.append(step)
+		if (str(rubik.get_state()) not in states):
+			states.add(str(rubik.get_state()))
+			tree.create_node(identifier = rubik, parent = parent_node_id, tag = {'steps': steps})
 
-	def init_phase(self, rubik, steps_required = []):
-		tree = Tree()
-		tree.add_node(Node(tag = {'state': deepcopy(rubik.get_state()), 'steps_required': steps_required}))
-		return tree
+def bfs(rubik, tree, phase, validate_state):
+	states = Set()
+	add_movements(tree, tree.root, phase, states)
+	q = deque([tree.get_node(tree.root)])
+	while q:
+		current = q.popleft()
+		for node in tree.children(current.identifier):
+			add_movements(tree, node.identifier, phase, states)
+			if (validate_state(current.identifier)):
+				return current.tag['steps']
+			q.append(node)
 
-	def execute(self, instructions, step):
-		for instruction in instructions[step]:
-			instruction()
-		return step
+def validate_phase_one(rubik):
+	return False
 
-
-	def add_state(self, tree, node_id, state, states, step):
-		steps = deepcopy(tree.get_node(node_id).tag['steps_required'])
-		if str(state) not in states:
-			steps.append(step)
-			states.add(str(state))
-			tree.create_node(parent=node_id, tag={'rubik': state, 'steps_required': steps})
-
-	def bfs(self, tree, moves, validate_state):
-		states = Set()
-		rubikHolder = deepcopy(self.rubik)
-		for move in moves.keys():
-			step = self.execute(moves, move)
-			self.add_state(tree, tree.root, self.rubik.get_state(), states, step)
-			self.rubik = rubikHolder
-		q = deque([tree.get_node(tree.root)])
-		while q:
-			current = q.popleft()
-			for node in tree.children(current.identifier):
-				rubikHolder = deepcopy(self.rubik)
-				for move in moves.keys():
-					step = self.execute(moves, move)
-					self.add_state(tree, current.identifier, self.rubik.get_state(), states, step)
-					self.rubik = rubikHolder
-					q.append(node)
-
-		print tree
+def phase_one(rubik, needed_moves, debug):
+	tree = init_phase(rubik)
+	return bfs(rubik, tree, 'phase_one', validate_phase_one)
 
 
-	def validate_phase_one(self, state):
-		return False
+def phase_two(rubik, debug):
+	tree = init_phase(rubik)
+	moves = []
 
-	def phase_one(self, rubik, needed_moves, debug):
-		tree = self.init_phase(rubik)
-		moves = []
+	#TODO add code here to solve phase two, and evaluate
 
-		self.bfs(tree, self.phase_one_moves, self.validate_phase_one)
+	return moves
 
-
-		#TODO add code here to solve phase one, and evaluate
-		return moves
-
-	def phase_two(self, rubik, debug):
-		tree = self.init_phase(rubik)
-		moves = []
-
-		#TODO add code here to solve phase two, and evaluate
-
-		return moves
-
-	def phase_two_edges(self, rubik, possible_moves, debug):
-		pass
+def phase_two_edges(rubik, possible_moves, debug):
+	pass
 
 
-	def phase_two_corners(self, rubik, possible_moves, debug):
-		pass
+def phase_two_corners(rubik, possible_moves, debug):
+	pass
 
-	def phase_three(self, rubik, debug):
-		tree = self.init_phase(rubik)
-		moves = []
+def phase_three(rubik, debug):
+	tree = init_phase(rubik)
+	moves = []
 
-		#TODO add code here to solve phase three, and evaluate
+	#TODO add code here to solve phase three, and evaluate
 
-		return moves
+	return moves
 
-	def phase_three_corners(self, rubik, possible_moves, debug):
-		pass
+def phase_three_corners(rubik, possible_moves, debug):
+	pass
 
-	def phase_three_edges(self, rubik, possible_moves, debug):
-		pass
+def phase_three_edges(rubik, possible_moves, debug):
+	pass
 
-	def phase_four(self, rubik, debug):
-		tree = init_phase(rubik)
-		moves = []
+def phase_four(rubik, debug):
+	tree = init_phase(rubik)
+	moves = []
 
-		#TODO add code here to solve phase four, and evaluate
+	#TODO add code here to solve phase four, and evaluate
 
-		return moves
+	return moves
 
 if __name__ == '__main__':
 	rubik = Rubik()
 	rubik.scramble()
-	solver = Solver(rubik)
-	moves = solver.solve(debug=True)
+	moves = solve(rubik, debug=True)
 	print moves
